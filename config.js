87226fccdcbb6c30c81cef170594497c02c3a526
shanulakshan <sasindulakshan2007binance@gmail.com>
2024-11-15 16:28:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "bqIV0bAD#CqNCFQ5H_t7VCs6X63c6SlVKpWRj5XNc6zDRcbUdAHc",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
